0eed6946269fa5ba906e5701d9b0e95369f204da
Charita Nichols <nichols.charita@gmail.com>
2020-06-03 16:31:18 -0400

    all passing

--- test/app.test.js
+++ test/app.test.js
@@ -1,39 +1,37 @@
-const { expect } = require('chai');
-const supertest = require('supertest');
-const app = require('../app');
-const playstore = require('../playstore');
+const { expect } = require("chai");
+const supertest = require("supertest");
+const app = require("../app");
+const playstore = require("../playstore");
 
-describe('GET /apps', () => {
-  it('should return 200 with json array of apps', () => {
+describe("GET /apps", () => {
+  it("should return 200 with json array of apps", () => {
     return supertest(app)
-      .get('/apps')
+      .get("/apps")
       .expect(200)
-      .expect('Content-Type', /json/)
-      .then(res => {
-        expect(res.body).to.be.an('array');
-        expect(res.body[0]).to.be.an('object');
-        expect(res.body[0]).to.include.keys(
-          'App', 'Category', 'Rating'
-        );
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        expect(res.body).to.be.an("array");
+        expect(res.body[0]).to.be.an("object");
+        expect(res.body[0]).to.include.keys("App", "Category", "Rating");
       });
   });
 
   it('should return 400 with "Sort must be one of App or rating"', () => {
     return supertest(app)
-      .get('/apps')
-      .query({ sort: 'invalid' })
-      .expect(400, 'Sort must be one of App or rating');
+      .get("/apps")
+      .query({ sort: "invalid" })
+      .expect(400, "Sort must be one of App or rating");
   });
 
-  const sortFilter = ['App', 'Rating'];
-  sortFilter.forEach(sort => {
+  const sortFilter = ["App", "Rating"];
+  sortFilter.forEach((sort) => {
     it(`should return apps data sorted by ${sort}`, () => {
       return supertest(app)
-        .get('/apps')
+        .get("/apps")
         .query({ sort: sort })
         .expect(200)
-        .then(res => {
-          expect(res.body).to.be.an('array');
+        .then((res) => {
+          expect(res.body).to.be.an("array");
           let sortArray = playstore.sort((a, b) => {
             return a[sort] - b[sort];
           });
@@ -41,6 +39,21 @@ describe('GET /apps', () => {
         });
     });
   });
-  
 
+  const genres = ["Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"];
+  genres.forEach((genre) => {
+    it(`should return apps data sorted by ${genre}`, () => {
+      return supertest(app)
+        .get("/apps")
+        .query({ genre: genre })
+        .expect(200)
+        .then((res) => {
+          expect(res.body).to.be.an("array");
+          let genreArray = playstore.filter((apps) =>
+            apps.Genres.toLowerCase().includes(genre.toLowerCase())
+          );
+          expect(genreArray).to.eql(res.body);
+        });
+    });
+  });
 });
